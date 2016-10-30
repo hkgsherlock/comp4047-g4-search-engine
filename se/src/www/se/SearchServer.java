a70692df92236e8537b34a1e233af845c55a0cb7
leoxyzboy9 <leoxyzboy9@gmail.com>
2016-10-31 01:43:03 +0800

    format

--- se/src/www/se/SearchServer.java
+++ se/src/www/se/SearchServer.java
@@ -15,7 +15,7 @@ public class SearchServer {
         System.out.print("<p>Hello World!</p>\n");
         System.out.print("Received query: " + Arrays.toString(args));
         System.out.print("testing");
-        Crawler crawler = new Crawler("http://hkbu.edu.hk/eng/main/index/jsp", 100, 10);
+        Crawler crawler = new Crawler("http://hkbu.edu.hk/eng/main/index.jsp", 100, 10);
         crawler.start();
         KeywordsStorage.INSTANCE.get("About").print();
     }
