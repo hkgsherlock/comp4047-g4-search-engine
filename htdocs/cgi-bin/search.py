2c5701fd41f837a0b2227711575df8b10b07ccec
hkgsherlock <hkgsherlock@gmail.com>
2016-10-31 04:30:25 +0800

    adding class paths

--- htdocs/cgi-bin/search.py
+++ htdocs/cgi-bin/search.py
@@ -18,4 +18,4 @@ import os
 os.chdir(class_dir)
 
 # invoke java search engine
-os.system('java ' + search_engine + ' ' + query)
+os.system('java -Dfile.encoding=UTF-8 -classpath "C:\\Program Files\\Java\\jdk1.8.0_25\\jre\\lib\\;.\\;javax.jms.jar;javax.annotation.jar;javax.persistence.jar;javax.servlet.jar;javax.servlet.jsp.jar;javax.transaction.jar;javax.resource.jar;javax.ejb.jar;javax.servlet.jsp.jstl.jar;jsoup-1.10.1.jar;boilerpipe-1.2.2.jar;nekohtml-1.9.20.jar;xercesImpl-2.11.0.jar;xml-apis-1.4.01.jar" ' + search_engine + ' ' + query)
